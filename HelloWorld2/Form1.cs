26a0b5b305c1da358e1262ebf8ded0f76d5c3d5b
Sackey718 <yuuki718@gmail.com>
2014-06-22 01:20:54 +0900

    メッセージボックス表示機能追加

--- HelloWorld2/Form1.cs
+++ HelloWorld2/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWorld2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HelloWorld", "");
+        }
     }
 }
